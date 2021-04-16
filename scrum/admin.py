21a4064039177c10e522d8ff3e757bf316cb9492
Danilo <daniloitj@gmail.com>
2021-04-16 12:52:49 -0300

    [cod] retirei search de scrum.admin

--- scrum/admin.py
+++ scrum/admin.py
@@ -3,7 +3,6 @@ from scrum.models import Project
 
 class ProjectAdminView(admin.ModelAdmin):
     list_display = ('name', 'sigla','description')
-    search_fields = ('name',)
     list_filter = ('sigla',)
 
 admin.site.register(Project, ProjectAdminView)
